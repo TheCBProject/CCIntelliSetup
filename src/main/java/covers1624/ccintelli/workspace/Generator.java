3d97c685f8bf56a1c16998736a7c0ea406ef0d98
covers1624 <laughlan.cov@internode.on.net>
2017-02-10 01:16:10 +1030

    method for brandon

---
+++ src/main/java/covers1624/ccintelli/workspace/Generator.java
@@ -0,0 +1,13 @@
+package covers1624.ccintelli.workspace;
+
+/**
+ * Created by covers1624 on 10/02/2017.
+ */
+public class Generator {
+
+    public static void goGoGadgetMakeTheIdeaThings() {
+
+    }
+    
+
+}
